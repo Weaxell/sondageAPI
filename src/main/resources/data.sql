7cb1c35ce8641c68c81305608df37616c299a609
weaxell <thomascarpentier5@gmail.com>
2020-12-10 20:26:43 +0100

    Connexion a la BD fonctionnelle

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO sondage VALUES(1, true, 'Sondage public');
-INSERT INTO sondage VALUES(2, false, 'Sondage prive');
+INSERT INTO sondages VALUES(1, true, 'Sondage public');
+INSERT INTO sondages VALUES(2, false, 'Sondage prive');
